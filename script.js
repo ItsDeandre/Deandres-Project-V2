6e15cbb053cd395fc292511135143d0217dace44
ItsDeandre <78035423+ItsDeandre@users.noreply.github.com>
2021-10-19 17:19:34 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,73 @@
+// When button 1 and 3 is clicked, "Wrong!" should fade in //
+$(".Door-1").click(function() {
+    $(".wrong-1").fadeIn();
+});
+$(".Door-3").click(function() {
+    $(".wrong-2").fadeIn();
+});
+// When button 2 is clicked all buttons and imgs should fade out //
+$(".Door-2").click(function() {
+    $(".buttons").fadeOut();
+});
+$(".Door-2").click(function() {
+    $(".doors").fadeOut();
+});
+$(".Door-2").click(function() {
+    $(".wrong-2").fadeOut();
+    $(".wrong-1").fadeOut();
+});
+//Page 2//
+$(".Door-2").click(function() {
+    $(".hallway").fadeIn();
+    $(".buttons2").fadeIn();
+    $(".choice").fadeIn();
+    $(".hidden").fadeIn();
+});
+$(".left").hover(function() {
+    $("body").css("background-color", "red");
+    $(".dont").toggle();
+    $(".hidden").fadeOut();
+});
+$(".right").hover(function() {
+    $("body").css("background-color", "lime");
+    $(".choice").fadeOut();
+    $(".do").toggle();
+    $(".hidden").fadeOut();
+});
+//IF PLAYER PICKS RIGHT//
+$(".right").click(function() {
+    $(".ending1").fadeIn();
+    $(".hallway").fadeOut();
+    $(".buttons2").fadeOut();
+});
+//IF PLAYER PICKS LEFT//
+$(".left").click(function() {
+    $(".ending2").fadeIn();
+    $(".hallway").fadeOut();
+    $(".buttons2").fadeOut();
+});
+$(".text2").hover(function() {
+    $(".secret").fadeIn();
+});
+//IF PLAYER FINDS SECRET ENDING //
+$(".hidden").hover(function() {
+$(".secret2").fadeIn();
+});
+$(".secret2").click(function() {
+    $(".hallway").fadeOut();
+    $(".buttons2").fadeOut();
+    $(".choice").fadeOut();
+    $(".hidden").fadeOut();
+    $(".secret2").fadeOut();
+    $(".text3").fadeIn();
+    $(".continue").fadeIn();
+});
+//PAGE 4//
+$(".continue").click(function() {
+    $(".text3").fadeOut();
+    $(".continue").fadeOut();
+    $(".tools").fadeIn();
+    $(".pickwp").fadeIn();
+});
+//DEANDRE TRY PUTTING THE WEAPON IMGS IN SEPERATE CLASSES INSTED OF DIV//
+    
